a87c5bda634e748b7327b3e0a590bc2772a43622
Brandon Yanofsky <byanofsky@me.com>
2024-10-10 20:49:14 -0700

    Read file and write using a buffer

--- cat/main.go
+++ cat/main.go
@@ -2,39 +2,66 @@
 package main
 
 import (
-	"flag"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 )
 
+const BUFFER_SIZE = 1000
+
 func main() {
-	// Define command-line flags
-	name := flag.String("name", "World", "Name to greet")
-	flag.Parse()
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: my-cat <file>\n")
+	}
 
-	// Use the parsed flag
-	fmt.Printf("Hello, %s!\n", *name)
+	file := os.Args[1]
 
-	// Example of handling subcommands
-	if len(os.Args) > 1 {
-		switch os.Args[1] {
-		case "version":
-			fmt.Println("v0.1.0")
-		case "help":
-			printHelp()
-		default:
-			fmt.Printf("Unknown command: %s\n", os.Args[1])
-			printHelp()
+	f, err := os.Open(file)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Fprintf(os.Stderr, "File does not exist: %s\n", file)
+		} else {
+			panic(err)
 		}
 	}
+	defer f.Close()
+
+	b := make([]byte, BUFFER_SIZE)
+	for {
+		n, err := f.Read(b)
+		if n == 0 && errors.Is(err, io.EOF) {
+			break
+		}
+		fmt.Fprintf(os.Stdin, "%s", b)
+	}
+	// // Define command-line flags
+	// name := flag.String("name", "World", "Name to greet")
+	// flag.Parse()
+
+	// // Use the parsed flag
+	// fmt.Printf("Hello, %s!\n", *name)
+
+	// // Example of handling subcommands
+	// if len(os.Args) > 1 {
+	// 	switch os.Args[1] {
+	// 	case "version":
+	// 		fmt.Println("v0.1.0")
+	// 	case "help":
+	// 		printHelp()
+	// 	default:
+	// 		fmt.Printf("Unknown command: %s\n", os.Args[1])
+	// 		printHelp()
+	// 	}
+	// }
 }
 
-func printHelp() {
-	fmt.Println("Usage:")
-	fmt.Println("  cli-tool [flags] [command]")
-	fmt.Println("\nFlags:")
-	flag.PrintDefaults()
-	fmt.Println("\nCommands:")
-	fmt.Println("  version    Print the version number")
-	fmt.Println("  help       Print this help message")
-}
+// func printHelp() {
+// 	fmt.Println("Usage:")
+// 	fmt.Println("  cli-tool [flags] [command]")
+// 	fmt.Println("\nFlags:")
+// 	flag.PrintDefaults()
+// 	fmt.Println("\nCommands:")
+// 	fmt.Println("  version    Print the version number")
+// 	fmt.Println("  help       Print this help message")
+// }
