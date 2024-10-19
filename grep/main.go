e15a7b9ba38fa67f37dd122cbf8fcf7c88bd784c
Brandon Yanofsky <byanofsky@me.com>
2024-10-18 20:43:02 -0700

    [grep] Refactor with claude

--- grep/main.go
+++ grep/main.go
@@ -12,6 +12,14 @@ import (
 	"strings"
 )
 
+type grepOptions struct {
+	recurse         bool
+	invert          bool
+	caseInsensitive bool
+	pattern         string
+	path            string
+}
+
 func parsePattern(pattern string) string {
 	p := pattern
 
@@ -23,33 +31,25 @@ func parsePattern(pattern string) string {
 	return p
 }
 
-func matchLine(pattern string, line string, caseInsensitive bool) bool {
-	// Check whether line matches
-	if len(pattern) == 0 { // Empty expression matches all
-		return true
-	}
+func compilePattern(pattern string, caseInsensitive bool) (*regexp.Regexp, error) {
 	if caseInsensitive {
-		pattern = strings.ToLower(pattern)
-		line = strings.ToLower(line)
+		pattern = "(?i)" + pattern
 	}
-	matched, err := regexp.MatchString(pattern, line)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Regexp error: %s\n", err)
-		os.Exit(1)
-	}
-	return matched
+	return regexp.Compile(pattern)
 }
 
-func grepFile(file string, pattern string, w *bufio.Writer, printFile bool, invertSearch bool, caseInsensitive bool) bool {
+func matchLine(re *regexp.Regexp, line string) bool {
+	return re.MatchString(line)
+}
+
+func grepFile(file string, re *regexp.Regexp, w *bufio.Writer, printFile, invert bool) (bool, error) {
 	// Open file
 	f, err := os.Open(file)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			fmt.Fprintf(os.Stderr, "File does not exist: %s\n", file)
-			os.Exit(1)
+			return false, fmt.Errorf("file does not exist: %s", file)
 		}
-		fmt.Fprintf(os.Stderr, "Error opening file: %s\n", err)
-		os.Exit(1)
+		return false, fmt.Errorf("error opening file %s: %w", file, err)
 	}
 	defer f.Close()
 
@@ -57,12 +57,11 @@ func grepFile(file string, pattern string, w *bufio.Writer, printFile bool, inve
 	scanner := bufio.NewScanner(f)
 	// Track whether match is found
 	foundMatch := false
-	// TODO: Swap to Reader.readline for longer lines
-	// See: https://stackoverflow.com/a/21124415
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		isMatch := matchLine(pattern, line, caseInsensitive)
-		if invertSearch {
+		isMatch := matchLine(re, line)
+		if invert {
 			isMatch = !isMatch
 		}
 		if !isMatch {
@@ -71,38 +70,31 @@ func grepFile(file string, pattern string, w *bufio.Writer, printFile bool, inve
 		foundMatch = true
 
 		// Write output
-		out := line
 		if printFile {
-			out = fmt.Sprintf("%s:%s", file, out)
-		}
-		out = fmt.Sprintf("%s\n", out)
-
-		n, err := w.WriteString(out)
-		if n != len(out) || err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing out: %s\n", err)
-			os.Exit(1)
+			fmt.Fprintf(w, "%s:%s\n", file, line)
+		} else {
+			fmt.Fprintln(w, line)
 		}
 	}
+
 	if err := scanner.Err(); err != nil {
-		// TODO: Extract to error checker function
-		fmt.Fprintf(os.Stderr, "Error printing: %s\n", err)
-		os.Exit(1)
+		return foundMatch, fmt.Errorf("error reading file %s: %w", file, err)
 	}
 
-	return foundMatch
+	return foundMatch, nil
 }
 
-func recurseGrep(w *bufio.Writer, invert bool, caseInsensitive bool) bool {
-	if flag.NArg() != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: grep -r <pattern> <file>")
+func recurseGrep(opts grepOptions, w *bufio.Writer) (bool, error) {
+	// TODO: Do this once
+	re, err := compilePattern(opts.pattern, opts.caseInsensitive)
+	if err != nil {
+		return false, fmt.Errorf("invalid regex pattern: %w", err)
 	}
-	pattern := parsePattern(flag.Arg(0))
-	root := flag.Arg(1)
 
 	foundMatch := false
 
 	// Walk the files from root. Skip dirs.
-	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+	err = filepath.WalkDir(opts.path, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -111,48 +103,80 @@ func recurseGrep(w *bufio.Writer, invert bool, caseInsensitive bool) bool {
 			return nil
 		}
 
-		if grepFile(path, pattern, w, true, invert, caseInsensitive) {
+		match, err := grepFile(path, re, w, true, opts.invert)
+		if err != nil {
+			return err
+		}
+		if match {
 			foundMatch = true
 		}
 		return nil
 	})
+
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error recursing path: %s\n", err)
+		return foundMatch, fmt.Errorf("error recursing path: %w", err)
 	}
 
-	return foundMatch
+	return foundMatch, nil
 }
 
-func grep(w *bufio.Writer, invert bool, caseInsensitive bool) bool {
-	if flag.NArg() != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: grep <pattern> <file>")
+func grep(opts grepOptions, w *bufio.Writer) (bool, error) {
+	// TODO: Compile pattern
+	re, err := compilePattern(opts.pattern, opts.caseInsensitive)
+	if err != nil {
+		return false, fmt.Errorf("invalid regex pattern: %w", err)
 	}
-	pattern := parsePattern(flag.Arg(0))
-	file := flag.Arg(1)
 
-	return grepFile(file, pattern, w, false, invert, caseInsensitive)
+	return grepFile(opts.path, re, w, false, opts.invert)
 }
 
-func main() {
+func run() error {
 	// Parse arguments
 	recurse := flag.Bool("r", false, "Recurse a directory.")
 	invert := flag.Bool("v", false, "Inverts search, excluding lines that match pattern.")
 	caseInsensitive := flag.Bool("i", false, "Makes pattern case insensitive.")
 	flag.Parse()
 
+	if flag.NArg() != 2 {
+		return errors.New("usage: my-grep [-r] [-v] [-i] <pattern> <file|irectory>")
+	}
+
+	opts := grepOptions{
+		recurse:         *recurse,
+		invert:          *invert,
+		caseInsensitive: *caseInsensitive,
+		pattern:         parsePattern(flag.Arg(0)),
+		path:            flag.Arg(1),
+	}
+
 	// Create buffered writer to Stdout
 	w := bufio.NewWriter(os.Stdout)
 	defer w.Flush()
 
 	var foundMatch bool
-	if *recurse {
-		foundMatch = recurseGrep(w, *invert, *caseInsensitive)
+	var err error
+
+	if opts.recurse {
+		foundMatch, err = recurseGrep(opts, w)
 	} else {
-		foundMatch = grep(w, *invert, *caseInsensitive)
+		foundMatch, err = grep(opts, w)
+	}
+
+	if err != nil {
+		return err
 	}
 
 	// Exit code 1 when match not found.
 	if !foundMatch {
+		return errors.New("no matches found")
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(1)
 	}
 }
