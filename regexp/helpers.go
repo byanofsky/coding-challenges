b33f93e790cdaa41ac94cb5bff57c32741f936b2
Brandon Yanofsky <byanofsky@me.com>
2024-10-21 20:36:46 -0700

    [regexp] Extract parsing to parse function

--- regexp/helpers.go
+++ regexp/helpers.go
@@ -40,6 +40,41 @@ func scan(pattern string) []token {
 	return tokens
 }
 
+// TODO: Return pointer to matcher
+func parse(tokens []token) ([]matcher, error) {
+	matchers := make([]matcher, 0)
+
+	i := 0
+	for i < len(tokens) {
+		token := tokens[i]
+		var matcher matcher
+
+		switch token.kind {
+		case SingleCharacter:
+			matcher = newSingleCharacterMatcher(token.token[0])
+		case Wildcard:
+			matcher = newWildcardMatcher()
+		case Repetition:
+			return nil, fmt.Errorf("possible reptition followed by repition. index: %d", i)
+		default:
+			return nil, fmt.Errorf("unknown pattern: %s. index: %d", token.token, i)
+		}
+
+		if i+1 < len(tokens) {
+			// Peak next char to check for repition
+			// TODO: Abstrac to is Reptition function
+			if tokens[i+1].token == string('*') {
+				matcher = newStarRepition(matcher)
+				i++
+			}
+		}
+
+		matchers = append(matchers, matcher)
+		i++
+	}
+	return matchers, nil
+}
+
 func isMatch(matchers []matcher, s string) (bool, error) {
 	var next nextMatcher
 	next = func(s string, i int, mIdx int) (bool, error) {
--- regexp/my_regexp.go
+++ regexp/my_regexp.go
@@ -1,43 +1,15 @@
 package my_regexp
 
-import "fmt"
-
 func (r Regexp) Match(s string) (bool, error) {
 	return isMatch(r.matchers, s)
 }
 
 func Compile(pattern string) (*Regexp, error) {
+	// TODO: Scan should return err
 	tokens := scan(pattern)
-
-	matchers := make([]matcher, 0)
-
-	i := 0
-	for i < len(tokens) {
-		token := tokens[i]
-		var matcher matcher
-
-		switch token.kind {
-		case SingleCharacter:
-			matcher = newSingleCharacterMatcher(token.token[0])
-		case Wildcard:
-			matcher = newWildcardMatcher()
-		case Repetition:
-			return nil, fmt.Errorf("possible reptition followed by repition. index: %d", i)
-		default:
-			return nil, fmt.Errorf("unknown pattern: %s. index: %d", token.token, i)
-		}
-
-		if i+1 < len(tokens) {
-			// Peak next char to check for repition
-			// TODO: Abstrac to is Reptition function
-			if tokens[i+1].token == string('*') {
-				matcher = newStarRepition(matcher)
-				i++
-			}
-		}
-
-		matchers = append(matchers, matcher)
-		i++
+	matchers, err := parse(tokens)
+	if err != nil {
+		return nil, err
 	}
 
 	re := Regexp{matchers: matchers}
