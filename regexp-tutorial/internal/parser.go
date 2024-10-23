a95b6b3312a47d7e68f04f7933a99a16de2dcf9f
Brandon Yanofsky <byanofsky@me.com>
2024-10-22 22:52:13 -0700

    [regexp-t] Replace parse string with StringParser

--- regexp-tutorial/internal/parser.go
+++ regexp-tutorial/internal/parser.go
@@ -6,13 +6,25 @@ import (
 	"unicode"
 )
 
+type Parser interface {
+	parse(s string) (string, error)
+}
+
+type StringParser struct {
+	prefix string
+}
+
+func (p StringParser) parse(s string) (after string, found bool) {
+	return strings.CutPrefix(s, p.prefix)
+}
+
+func NewStringParser(prefix string) *StringParser {
+	return &StringParser{prefix: prefix}
+}
+
 type RangeQuantifier struct {
 	lowerBound int
 	upperBound int
-}
-
-func parseString(prefix string, s string) (after string, found bool) {
-	return strings.CutPrefix(s, prefix)
 }
 
 func parseNumber(s string) (result int, after string, found bool) {
@@ -41,7 +53,7 @@ Returns a tuple of three values:
 2 and 3 are returned iff a match is found.
 */
 func parseRangeQuantifier(s string) (match bool, rq *RangeQuantifier, after string) {
-	after, found := parseString("{", s)
+	after, found := NewStringParser("{").parse(s)
 	if !found {
 		return false, nil, s
 	}
@@ -51,7 +63,7 @@ func parseRangeQuantifier(s string) (match bool, rq *RangeQuantifier, after stri
 		return false, nil, s
 	}
 
-	after, found = parseString("}", after)
+	after, found = NewStringParser("}").parse(after)
 	if !found {
 		return false, nil, s
 	}
