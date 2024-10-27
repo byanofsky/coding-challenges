4460dcf5e8313f9ded47e9d9282a93b388e9babd
Brandon Yanofsky <byanofsky@me.com>
2024-10-27 09:04:53 -0700

    [videochat] 1: basic http route

--- video-chat-app/main.go
+++ video-chat-app/main.go
@@ -1,7 +1,15 @@
-package videochatapp
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
