49ff2db655925ff1d429783bae1cd3a80842c003
Brandon Yanofsky <byanofsky@me.com>
2024-11-06 21:18:29 -0800

    [redis] 1: Simple string array

--- redis/internal/serialize.go
+++ redis/internal/serialize.go
@@ -2,15 +2,22 @@ package internal
 
 import (
 	"fmt"
+	"reflect"
 )
 
 type Null struct{}
 
 type Serializable interface {
-	Null | int | string
+	Null | int | string | []any
 }
 
-func Serialize[T Serializable](data T) (string, error) {
+func Serialize(data any) (string, error) {
+	fmt.Println(reflect.TypeOf(data).Kind())
+	if reflect.TypeOf(data).Kind() == reflect.Slice {
+		a := any(data).([]any)
+		return serializeArray(a)
+	}
+
 	switch t := any(data).(type) {
 	case Null:
 		return serializeNull(), nil
@@ -30,9 +37,24 @@ func serializeNull() string {
 }
 
 func serializeString(s string) string {
+	// TODO: The string mustn't contain a CR (\r) or LF (\n) character and is terminated by CRLF (i.e., \r\n).
 	return fmt.Sprintf("+%s\r\n", s)
 }
 
 func serializeInt(i int) string {
 	return fmt.Sprintf(":%d\r\n", i)
+}
+
+func serializeArray(a []any) (string, error) {
+	result := fmt.Sprintf("*%d\r\n", len(a))
+
+	for _, item := range a {
+		s, err := Serialize(item)
+		if err != nil {
+			return "", err
+		}
+		result += s
+	}
+
+	return result, nil
 }
--- redis/internal/serialize_test.go
+++ redis/internal/serialize_test.go
@@ -6,8 +6,16 @@ import (
 
 func TestSerialize(t *testing.T) {
 	runSerializeTest(t, "Null", Null{}, "$-1\r\n")
-	runSerializeTest(t, "String", "OK", "+OK\r\n")
+	runSerializeTest(t, "SimpleString1", "OK", "+OK\r\n")
+	runSerializeTest(t, "SimpleString2", "hello world", "+hello world\r\n")
 	runSerializeTest(t, "Int", 5, ":5\r\n")
+	runSerializeTest(t, "Array SimpleString", []any{"ping"}, "*1\r\n+ping\r\n")
+	// runSerializeTest(t, "Array", []any{"ping"}, "*1\r\n$4\r\nping\r\n")
+	// runSerializeTest(t, "", "", "*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n")
+	// runSerializeTest(t, "", "", "*2\r\n$3\r\nget\r\n$3\r\nkey\r\n")
+	// runSerializeTest(t, "", "", "+OK\r\n")
+	// runSerializeTest(t, "", "", "-Error message\r\n")
+	// runSerializeTest(t, "", "", "$0\r\n\r\n")
 }
 
 func runSerializeTest[T Serializable](t *testing.T, name string, input T, want string) {
@@ -15,11 +23,11 @@ func runSerializeTest[T Serializable](t *testing.T, name string, input T, want s
 		got, err := Serialize(input)
 
 		if err != nil {
-			t.Fatalf("input %v, unexpected error: %v", input, err)
+			t.Fatalf("input %q, unexpected error: %v", input, err)
 		}
 
 		if got != want {
-			t.Fatalf("input %v, want %v, got %v", input, want, got)
+			t.Fatalf("input %q, want %q, got %q", input, want, got)
 		}
 	})
 }
