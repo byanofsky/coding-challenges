a9f82cfd76d93b633389559a804fadf074bcd9b9
Brandon Yanofsky <byanofsky@me.com>
2024-11-07 20:13:21 -0800

    [redis] 1: Refactor with value struct

--- redis/internal/serialize.go
+++ redis/internal/serialize.go
@@ -2,33 +2,97 @@ package internal
 
 import (
 	"fmt"
-	"reflect"
 )
 
-type Null struct{}
+type Kind int
 
-type Serializable interface {
-	Null | int | string | []any
+const (
+	NullKind Kind = iota
+	StringKind
+	IntKind
+	ArrayKind
+)
+
+func (k Kind) String() string {
+	switch k {
+	case NullKind:
+		return "Null"
+	case StringKind:
+		return "String"
+	case IntKind:
+		return "Int"
+	case ArrayKind:
+		return "Array"
+	default:
+		return "Unknown"
+	}
 }
 
-func Serialize(data any) (string, error) {
-	fmt.Println(reflect.TypeOf(data).Kind())
-	if reflect.TypeOf(data).Kind() == reflect.Slice {
-		a := any(data).([]any)
-		return serializeArray(a)
-	}
+type Data struct {
+	kind  Kind
+	value interface{}
+}
 
-	switch t := any(data).(type) {
-	case Null:
-		return serializeNull(), nil
-	case string:
-		s := any(data).(string)
-		return serializeString(s), nil
-	case int:
-		i := any(data).(int)
-		return serializeInt(i), nil
+func (d Data) String() string {
+	switch d.kind {
+	case NullKind:
+		return "Data{Null}"
+	case StringKind:
+		return fmt.Sprintf("Data{%q}", d.value)
+	case IntKind:
+		return fmt.Sprintf("Data{%d}", d.value)
+	case ArrayKind:
+		return fmt.Sprintf("Data{%v}", d.value)
 	default:
-		return "", fmt.Errorf("error unexpected data type: %v", t)
+		return "Unknown"
+	}
+}
+
+func (d Data) GetString() (string, error) {
+	if d.kind != StringKind {
+		return "", fmt.Errorf("cannot GetString of kind: %s", d.kind)
+	}
+	s, ok := d.value.(string)
+	if !ok {
+		return "", fmt.Errorf("error value is not a string: %v", d.value)
+	}
+	return s, nil
+}
+
+func (d Data) GetInt() (int, error) {
+	if d.kind != IntKind {
+		return 0, fmt.Errorf("cannot GetInt of kind: %s", d.kind)
+	}
+	i, ok := d.value.(int)
+	if !ok {
+		return 0, fmt.Errorf("error value is not an int: %v", d.value)
+	}
+	return i, nil
+}
+
+func (d Data) GetArray() (*[]Data, error) {
+	if d.kind != ArrayKind {
+		return nil, fmt.Errorf("cannot GetArray of kind: %s", d.kind)
+	}
+	a, ok := d.value.([]Data)
+	if !ok {
+		return nil, fmt.Errorf("error value is not an array: %v", d.value)
+	}
+	return &a, nil
+}
+
+func Serialize(data Data) (string, error) {
+	switch data.kind {
+	case ArrayKind:
+		return serializeArray(data)
+	case NullKind:
+		return serializeNull(), nil
+	case StringKind:
+		return serializeString(data)
+	case IntKind:
+		return serializeInt(data)
+	default:
+		return "", fmt.Errorf("error unexpected data type: %v", data.kind)
 	}
 }
 
@@ -36,19 +100,32 @@ func serializeNull() string {
 	return "$-1\r\n"
 }
 
-func serializeString(s string) string {
+func serializeString(d Data) (string, error) {
+	s, err := d.GetString()
+	if err != nil {
+		return "", fmt.Errorf("error serializing string: %w", err)
+	}
 	// TODO: The string mustn't contain a CR (\r) or LF (\n) character and is terminated by CRLF (i.e., \r\n).
-	return fmt.Sprintf("+%s\r\n", s)
+	return fmt.Sprintf("+%s\r\n", s), nil
 }
 
-func serializeInt(i int) string {
-	return fmt.Sprintf(":%d\r\n", i)
+func serializeInt(d Data) (string, error) {
+	i, err := d.GetInt()
+	if err != nil {
+		return "", fmt.Errorf("error serializing int: %w", err)
+	}
+	return fmt.Sprintf(":%d\r\n", i), nil
 }
 
-func serializeArray(a []any) (string, error) {
-	result := fmt.Sprintf("*%d\r\n", len(a))
+func serializeArray(d Data) (string, error) {
+	a, err := d.GetArray()
+	if err != nil {
+		return "", fmt.Errorf("error serializing array: %w", err)
+	}
 
-	for _, item := range a {
+	result := fmt.Sprintf("*%d\r\n", len(*a))
+
+	for _, item := range *a {
 		s, err := Serialize(item)
 		if err != nil {
 			return "", err
--- redis/internal/serialize_test.go
+++ redis/internal/serialize_test.go
@@ -5,11 +5,11 @@ import (
 )
 
 func TestSerialize(t *testing.T) {
-	runSerializeTest(t, "Null", Null{}, "$-1\r\n")
-	runSerializeTest(t, "SimpleString1", "OK", "+OK\r\n")
-	runSerializeTest(t, "SimpleString2", "hello world", "+hello world\r\n")
-	runSerializeTest(t, "Int", 5, ":5\r\n")
-	runSerializeTest(t, "Array SimpleString", []any{"ping"}, "*1\r\n+ping\r\n")
+	runSerializeTest(t, "Null", Data{kind: NullKind}, "$-1\r\n")
+	runSerializeTest(t, "SimpleString1", Data{kind: StringKind, value: "OK"}, "+OK\r\n")
+	runSerializeTest(t, "SimpleString2", Data{kind: StringKind, value: "hello world"}, "+hello world\r\n")
+	runSerializeTest(t, "Int", Data{kind: IntKind, value: 5}, ":5\r\n")
+	runSerializeTest(t, "Array SimpleString", Data{kind: ArrayKind, value: []Data{{kind: StringKind, value: "ping"}}}, "*1\r\n+ping\r\n")
 	// runSerializeTest(t, "Array", []any{"ping"}, "*1\r\n$4\r\nping\r\n")
 	// runSerializeTest(t, "", "", "*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n")
 	// runSerializeTest(t, "", "", "*2\r\n$3\r\nget\r\n$3\r\nkey\r\n")
@@ -18,16 +18,16 @@ func TestSerialize(t *testing.T) {
 	// runSerializeTest(t, "", "", "$0\r\n\r\n")
 }
 
-func runSerializeTest[T Serializable](t *testing.T, name string, input T, want string) {
+func runSerializeTest(t *testing.T, name string, input Data, want string) {
 	t.Run(name, func(t *testing.T) {
 		got, err := Serialize(input)
 
 		if err != nil {
-			t.Fatalf("input %q, unexpected error: %v", input, err)
+			t.Fatalf("input %v, unexpected error: %v", input, err)
 		}
 
 		if got != want {
-			t.Fatalf("input %q, want %q, got %q", input, want, got)
+			t.Fatalf("input %v, want %q, got %q", input, want, got)
 		}
 	})
 }
